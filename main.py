41b42820aa07871cd9b5273e51ab48e4526ae27f
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-09 23:31:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 # ******************************
 # Make your Code
 # ******************************
+numbers = input().split()
+
+# converting ints to strings
+for i in range(len(numbers)):
+    numbers[i] = int(numbers[i])
+
+for j in range(len(numbers)):
+    minValue = numbers[j]
+    for k in range(j, len(numbers)):
+        if minValue > numbers[k]:
+            minValue = numbers[k]
+    
+    # swapping the min and current value
+    numbers[numbers.index(minValue)] = numbers[j]
+    numbers[j] = minValue
+
+    
+
+    # print()
+    # printing the swap progress
+    print(numbers)    
